1458baf290a0dbd969652293d6ae8e0686b7040b
etnite <brian.ward2@usda.gov>
2020-02-28 16:04:51 -0600

    Working draft of samtools_depth_calc.sh

--- code/bam_depth_calc/samtools_depth_calc.sh
+++ code/bam_depth_calc/samtools_depth_calc.sh
@@ -17,7 +17,7 @@ set -e
 #SBATCH --nodes=1 #Number of nodes
 #SBATCH --ntasks=1  #Number of overall tasks - overrides tasks per node
   ##SBATCH --ntasks-per-node=22 #number of cores/tasks
-#SBATCH --time=12:00:00 #time allocated for this job hours:mins:seconds
+#SBATCH --time=1:00:00 #time allocated for this job hours:mins:seconds
 #SBATCH --mail-user=jane.doe@isp.com #enter your email address to receive emails
 #SBATCH --mail-type=BEGIN,END,FAIL #will receive an email when job starts, ends or fails
 #SBATCH --output="stdout.%j.%N" # standard out %j adds job number to outputfile name and %N adds the node name
@@ -29,38 +29,40 @@ set -e
 ## If you want to use all .bam files in a directory, point bam_dir to that
 ## directory, and set bam_list to a string that doesn't point to any file
 bam_list="none"
-bam_dir=""
+bam_dir="/project/genolabswheatphg/alignments/ERSGGL_SRW_bw2_bams/SRW_merged_excap_GBS_wholechrom_bw2_bams"
 
 ## minimum read quality to be included
 ## maximum number of reads to count for a position in EACH .bam file
 ## minimum depth summed ACROSS .bams for a position to be printed
 ## Maximum gap for bedtools merge
 mq_min=20
-max_dep=100
+max_dep=200
 min_dep=50
 max_gap=100
 
-out_bed=""
+out_bed="/home/brian.ward/test_out.bed"
 
 
 #### Executable ####
 
+module load samtools
 module load bedtools
 
 ## Write the list of .bam files
-out_dir=$(dirname "$out_file")
+out_dir=$(dirname "$out_bed")
+echo "$out_dir"
 if [[ -f "$bam_list" ]]; then
     cp "$bam_list" "$out_dir"/temp_bam_list.txt
 else
-    ls -1 *.bam > "$out_dir"/temp_bam_list.txt
+    ls -1 "$bam_dir"/*.bam > "$out_dir"/temp_bam_list.txt
 fi
 
 ## Perform depth calculation
 samtools depth -Q $mq_min -f "$out_dir"/temp_bam_list.txt |
-    awk '{for(i=3;i<=NF;i++) sum+=; print($1,$2-1,$2,sum); sum=0}' |
-    awk -v md=$min_dep 'BEGIN {OFS = "\t"} $4 > md' |
-    head -n 100000 | # For testing
-    bedtools merge -d $max_gap -i stdin > "$out_bed"
+    awk '{sum=0; for(i=3; i<=NF; i++) { sum+= $i } print($1,$2-1,$2,sum)}' |
+    awk -v md=$min_dep 'BEGIN {OFS = "\t"} $4 > md {print($1, $2, $3, $4)}' | head -n 1000 > "$out_dir"/temp.bed
+ 
+bedtools merge -d $max_gap -i "$out_dir"/temp.bed > "$out_bed"
 
 ## Run the depth calculation
 #samtools depth -Q $mq_min -f "$out_dir"/temp_bam_list.txt | 
