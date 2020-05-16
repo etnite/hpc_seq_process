943db88daf87eaf7363bc5b143d8b5b9e9ec5b92
etnite <brian.ward2@usda.gov>
2020-05-16 16:27:48 -0500

    Some changes to ubam_to_bam.sh, but piping still fails

--- code/gatk_preprocessing/ubam_to_bam.sh
+++ code/gatk_preprocessing/ubam_to_bam.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#### WARNING: Under Development ####
+
 ## Convert a uBAM file to a BAM
 ##
 ## This script is designed to take an unmapped BAM file, and then:
@@ -16,13 +18,16 @@
 ## of sample names, one listed per line
 in_dir="/project/genolabswheatphg/merged_fastqs/SRW_excap/fq_demux_test"
 mark_dir="/project/genolabswheatphg/merged_fastqs/SRW_excap/fq_demux_test/marked_adapters"
-#out_dir="/project/genolabswheatphg/merged_fastqs/SRW_excap/fq_demux_test"
+out_dir="/project/genolabswheatphg/merged_fastqs/SRW_excap/fq_demux_test/aligned"
 #bams_file="/home/brian.ward/repos/wheat_phg/sample_lists/v1_hapmap_bioproj/sample_names.txt"
+
+ref="/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.dna.toplevel.fa"
 
 
 #### Executable ####
 
 module load bowtie2
+module load bwa
 module load samtools
 source activate gatk4
 
@@ -40,30 +45,28 @@ mkdir -p "$mark_dir"
 in_bam="TRIBUTE_sub10K_L001.bam"
 mark_bam=$(echo "$in_bam" | sed 's/.bam/markadapters.bam/')
 met_file=$(echo "$in_bam" | sed 's/.bam/markadapters_metrics.txt/')
+out_bam=$(echo "$in_bam" | sed 's/.bam/_aligned.bam/')
 
 ## Mark Illumina adapters in uBAM
 gatk MarkIlluminaAdapters \
 	-I "$in_dir"/"$in_bam" \
 	-O "$mark_dir"/"$mark_bam" \
 	-M "$mark_dir"/"$met_file" \
-	TMP_DIR="$mark_dir"
-
-source deactivate
-echo
-echo "End time:"
-date
-
-exit 0;
+	--TMP_DIR "$mark_dir"
 
 ## Pipe to run SamtoFastq, followed by alignment, followed by MergeBamAlignment
+gatk SamToFastq \
+    -I "$mark_dir"/"$mark_bam" \
+    --FASTQ /dev/stdout \
+    --CLIPPING_ATTRIBUTE XT \
+    --CLIPPING_ACTION 2 \
+    --INTERLEAVE true \
+    --INCLUDE_NON_PF_READS true \
+    --TMP_DIR "$out_dir" |
 
-gatk -Xmx8G -jar SamToFastq \
-I=6483_snippet_markilluminaadapters.bam \
-FASTQ=/dev/stdout \
-CLIPPING_ATTRIBUTE=XT CLIPPING_ACTION=2 INTERLEAVE=true NON_PF=true \
-TMPDIR=/path/shlee | \
-/path/bwa mem -M -t 7 -p /path/Homosapiens_assembly19.fasta /dev/stdin | \
+bwa mem -M -t 1 -p "$ref" "$out_dir"/"$out_bam"
 
+exit 0;
 
 java -Xmx16G -jar /path/picard.jar MergeBamAlignment \
 ALIGNED_BAM=/dev/stdin \
