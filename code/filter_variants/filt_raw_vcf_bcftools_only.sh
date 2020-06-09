149c372fefe20ed93f486d652eb462e6ea762769
etnite <brian.ward2@usda.gov>
2020-06-09 13:52:52 -0500

    Added plotting to filt_raw_vcf_bcftools_only.sh

--- code/filter_variants/filt_raw_vcf_bcftools_only.sh
+++ code/filter_variants/filt_raw_vcf_bcftools_only.sh
@@ -27,6 +27,12 @@ set -e
 ##
 ## The script will output to either a .bcf file or .vcf.gz depending on the supplied
 ## file extension 
+##
+## The script will then create summary statistics using bcftools stats, and will
+## attempt to create plots of these statistics using the plot-vcfstats script that
+## is bundled with bcftools. Python 3 and matplotlib are required for this step.
+## If they aren't installed, an error will be printed, but it will not otherwise
+## effect the output of the script.
 ################################################################################
 
 
@@ -37,7 +43,7 @@ set -e
   ##SBATCH --nodes=1 #Number of nodes
 #SBATCH --ntasks=1  #Number of overall tasks - overrides tasks per node
   ##SBATCH --ntasks-per-node=22 #number of cores/tasks
-#SBATCH --time=4:00:00 #time allocated for this job hours:mins:seconds
+#SBATCH --time=5:00:00 #time allocated for this job hours:mins:seconds
 #SBATCH --mail-user=bpward2@ncsu.edu #enter your email address to receive emails
 #SBATCH --mail-type=BEGIN,END,FAIL #will receive an email when job starts, ends or fails
 #SBATCH --output="stdout.%j.%N" # standard out %j adds job number to outputfile name and %N adds the node name
@@ -53,7 +59,7 @@ vcf_out="/project/guedira_seq_map/brian/US_excap/v1_variants/US_excap_filt08_var
 taxa_list="none"
 min_maf=0.05
 max_miss=0.8
-max_het=0.1
+max_het=1
 min_dp=0
 max_dp=1e6
 remove_unal="false"
@@ -64,7 +70,10 @@ indelgap=3
 
 #### Executable #####
 
-module load bcftools
+## Using conda here to utilize bcftools plugins and plot-vcfstats
+#module load bcftools
+module load miniconda
+source activate htslib
 
 echo
 echo "Start time:"
@@ -85,6 +94,7 @@ fi
 ## Get first letter of true/false options
 remove_unal="${remove_unal:0:1}"
 het2miss="${het2miss:0:1}"
+if [[ "$het2miss" == [Tt] ]]; then max_het="NA"; fi
 
 ## Create output directory (if necessary) and temp directory
 out_dir=$(dirname "${vcf_out}")
@@ -105,6 +115,7 @@ echo -e "Max het. proportion\t${max_het}" >> "${out_dir}"/filtering_params.txt
 echo -e "Min. average depth\t${min_dp}" >> "${out_dir}"/filtering_params.txt
 echo -e "Max average depth\t${max_dp}" >> "${out_dir}"/filtering_params.txt
 echo -e "Unaligned contigs removed?\t${remove_unal}" >> "${out_dir}"/filtering_params.txt
+echo -e "Heterozygous calls converted to missing data?\t${het2miss}" >> "${out_dir}"/filtering_params.txt
 echo -e "SNP overlap gap\t${snpgap}" >> "${out_dir}"/filtering_params.txt
 echo -e "Indel overlap gap\t${indelgap}" >> "${out_dir}"/filtering_params.txt
 
@@ -116,6 +127,7 @@ else
     bcftools query --list-samples $vcf_in > "${temp_dir}"/taxa_list.txt
 fi
 
+
 ## Big if-else block for different actions depending on remove_unal and het2miss
 echo "Filtering VCF..."
 echo
@@ -123,10 +135,9 @@ if [[ "$remove_unal" == [Tt] && "$het2miss" == [Tt] ]]; then
     bcftools view "${vcf_in}" \
         --samples-file "${temp_dir}"/taxa_list.txt \
         --output-type u |
-    bcftools +setGT - -- --targets q \
+    bcftools +setGT --output-type u - -- --target-gt q \
         --include 'GT="het"' \
-        --new-gt "./." \
-        --output-type u |
+        --new-gt "./." |
     bcftools view - \
         --targets ^UN,Un \
         --exclude "F_MISSING > ${max_miss} || MAF < ${min_maf} || INFO/DP < ${min_dp} || INFO/DP > ${max_dp}" \
@@ -151,10 +162,9 @@ elif [[ $remove_unal == [Ff] && "$het2miss" == [Tt] ]]; then
     bcftools view "${vcf_in}" \
         --samples-file "${temp_dir}"/taxa_list.txt \
         --output-type u |
-    bcftools +setGT - -- --targets q \
+    bcftools +setGT --output-type u - -- --target-gt q \
         --include 'GT="het"' \
-        --new-gt "./." \
-        --output-type u |
+        --new-gt "./." |
     bcftools view - \
         --exclude "F_MISSING > ${max_miss} || MAF < ${min_maf} || INFO/DP < ${min_dp} || INFO/DP > ${max_dp}" \
         --output-type u |
@@ -183,6 +193,9 @@ bcftools index -c "${vcf_out}"
 ## Generate summary stats
 bcftools stats "${vcf_out}" > "${base}_stats.txt"
 
+## Create plots from summary stats
+mkdir "${base}_plots"
+plot-vcfstats --prefix "${base}_plots" --no-PDF "${base}_stats.txt"
 
 ## Generate summary stats using TASSEL
 #echo "Generating summary statistics with TASSEL..."
