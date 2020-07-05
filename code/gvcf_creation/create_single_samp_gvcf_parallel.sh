26c21d5cecc60b578df5cbe06b94dcbab87dfc9b
etnite <brian.phillip.ward@gmail.com>
2020-07-04 20:41:31 -0400

    updated gVCF creation script

--- code/gvcf_creation/create_single_samp_gvcf_parallel.sh
+++ code/gvcf_creation/create_single_samp_gvcf_parallel.sh
@@ -17,8 +17,9 @@ set -e
 ##
 ## And go through the prompts.
 ##
-## This script assumes that BAM files are all located within a single directory,
-## and that they labeled in the form: sample_name.bam
+## This script takes an input text file listing paths to .bam files, one per line
+## It is designed to work with arrayer.sh, which will dispatch this script to run
+## on one of the .bam files listed in the input text file. 
 ##
 ## WARNING: GATK's HaplotypeCaller is memory-hungry, and will easily overrun
 ## individual core memory limits if not reigned in. Memory limits should be
@@ -27,16 +28,13 @@ set -e
 ##   gatk --java-options "-Xmx6g" HaplotypeCaller ...
 ##
 ## will limit memory usage to 6GB.
-##
-## In addition, this script is designed to work with arrayer.sh, to perform the 
-## gVCF conversion on multiple samples simultaneously.
 ################################################################################
 
 
 #### User-defined Constants ####
 
 ref_gen="/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.dna.toplevel.fa"
-in_dir="/project/genolabswheatphg/alignments/ERSGGL_SRW_bw2_bams/SRW_merged_excap_GBS_wholechrom_bw2_bams_mq20_filt"
+bam_list="/project/genolabswheatphg/alignments/ERSGGL_SRW_bw2_bams/SRW_merged_excap_GBS_wholechrom_bw2_bams_mq20_filt"
 out_dir="/project/genolabswheatphg/gvcfs/SRW_single_samp_bw2_excap_GBS_mq20"
 
 
@@ -46,26 +44,24 @@ module load tabix
 module load miniconda
 source activate gatk
 
-mkdir -p "${out_dir}"
+mkdir -p "$out_dir"
 
 ## Read in array index (integer) - get corresponding .bam file name
 ## Generate output file name
 arr_ind=$1
-bam_file=$(ls -1 "${in_dir}"/*.bam | head -n $arr_ind | tail -n 1)
-bam_base=$(basename "${bam_file}")
+bam_file=$(head -n $arr_ind "$bam_list" | tail -n 1)
+bam_base=$(basename "$bam_file")
 samp="${bam_base%.*}"
-out_file="${out_dir}"/"${samp}".g.vcf
+out_file="${out_dir}/${samp}.g.vcf"
 
 ## Run haplotype caller
 gatk --java-options "-Xmx3g" HaplotypeCaller \
-     -R "${ref_gen}" \
-     -I "${bam_file}" \
-     --read-index "${bam_file}".csi \
-     -ERC GVCF \
-     -O "${out_file}"
+     --reference "$ref_gen" \
+     --input "$bam_file" \
+     --read-index "${bam_file}.csi" \
+     --emit-ref-confidence GVCF \
+     --output "$out_file"
 
 ## Compress and index
-bgzip "${out_file}"
-tabix --csi --preset vcf "${out_file}".gz 
-
-source deactivate
+bgzip "$out_file"
+tabix --csi --preset vcf "${out_file}.gz" 
