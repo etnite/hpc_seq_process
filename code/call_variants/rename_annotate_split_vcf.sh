83934af016a877f932eb04f1f09144c0ec71e83b
etnite <brian.phillip.ward@gmail.com>
2020-10-05 09:07:28 -0400

    Small changes to rename_annotate_split_vcf.sh

--- code/call_variants/rename_annotate_split_vcf.sh
+++ code/call_variants/rename_annotate_split_vcf.sh
@@ -33,8 +33,8 @@
   ##SBATCH --nodes=1 #Number of nodes
 #SBATCH --ntasks=1  #Number of overall tasks - overrides tasks per node
   ##SBATCH --ntasks-per-node=22 #number of cores/tasks
-#SBATCH --time=02:00:00 #time allocated for this job hours:mins:seconds
-#SBATCH --mail-user=jane.doe@isp.com #enter your email address to receive emails
+#SBATCH --time=08:00:00 #time allocated for this job hours:mins:seconds
+#SBATCH --mail-user=bpward2@ncsu.edu #enter your email address to receive emails
 #SBATCH --mail-type=BEGIN,END,FAIL #will receive an email when job starts, ends or fails
 #SBATCH --output="stdout.%j.%N" # standard out %j adds job number to outputfile name and %N adds the node name
 #SBATCH --error="stderr.%j.%N" #optional but it prints our standard error
@@ -42,9 +42,9 @@
 
 #### User-defined constants ####
 
-vcf_in="/project/genolabswheatphg/variants/KS_HRW/filt_vcf/KS_HRW_filt.vcf.gz"
-ref="/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.dna.toplevel.fa"
-gff="/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.44.gff3.gz"
+vcf_in="/lustre/project/genolabswheatphg/US_excap/v1_variants/brian_raw_v1_variants/v1_raw_variants.bcf"
+ref="/lustre/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.dna.toplevel.fa"
+gff="/lustre/project/genolabswheatphg/v1_refseq/whole_chroms/Triticum_aestivum.IWGSC.44.gff3.gz"
 lookup_file="none"
 split_file="false"
 
@@ -57,17 +57,16 @@ echo
 echo "Start time:"
 date
 
-## Get the output format and file extension
+## Isolate the file format and extension
 out_dir=$(dirname "$vcf_in")
 ext="${vcf_in##*.}"
 base="${vcf_in%.*}"
 base="${vcf_in%.*}"
 if [[ "$ext" == "gz" ]]; then
-    out_fmt="z"
-    out_ext=".vcf.gz"
+    fmt="z"
+    ext="vcf.gz"
 elif [[ "$ext" == "bcf" ]]; then
-    out_fmt="b"
-    out_ext=".bcf"
+    fmt="b"
 else
     echo
     echo "Input file should be either in BCF (.bcf) or gzipped VCF (.vcf.gz) format"
@@ -78,22 +77,25 @@ fi
 ## If sample name lookup file is supplied, then replace sample names
 ## and call consequences. Otherwise, skip sample renaming step
 if [[ -f "$lookup_file" ]]; then
-	bcftools reheader --samples "$lookup_file" "$vcf_in" |
-        bcftools csq -f "$ref" -g "$gff" -p a -O "$out_fmt" -o "${base}_csq${out_ext}"
+	bcftools reheader "$vcf_in" --samples "$lookup_file" |
+        bcftools csq -f "$ref" -g "$gff" -p a -O "$fmt" -o "${base}_csq.${ext}"
 else
-    bcftools "$vcf_in" csq -f "$ref" -g "$gff" -p a -O "$out_fmt" -o "${base}_csq${out_ext}"
+    bcftools csq "$vcf_in" -f "$ref" -g "$gff" -p a -O "$fmt" -o "${base}_csq.${ext}"
 fi
 
-bcftools index -c "${base}_csq${out_ext}"
+bcftools index -c "${base}_csq.${ext}"
 
 ## Create SNP-only and indel-only files if the user specified
 split_file="${split_file:0:1}"
 split_file="${split_file^^}"
 if [[ "$split_file" == "T" ]]; then
-    bcftools view --type snps -O "$out_fmt" -o "${base}_csq_snps_only${out_ext}" "${base}_csq${out_ext}"
-    bcftools index -c "${base}_csq_snps_only${out_ext}"
-    bcftools view --type indels -O "$out_fmt" -o "${base}_csq_indels_only${out_ext}" "${base}_csq${out_ext}"
-    bcftools index -c "${base}_csq_indels_only${out_ext}"
+    bcftools view "${base}_csq.${ext}" --type snps -O "$fmt" -o "${base}_csq_snps_only.${ext}"
+    bcftools index -c "${base}_csq_snps_only.${ext}"
+    bcftools view "${base}_csq.${ext}" --type indels -O "$fmt" -o "${base}_csq_indels_only.${ext}"
+    bcftools index -c "${base}_csq_indels_only.${ext}"
+else
+    echo
+    echo "User selected not to split SNPs and indels into separate files"
 fi
 
 echo
