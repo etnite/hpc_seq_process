c13ea0313c639c01ae09207212b50e746f93006d
bward8 <brian.phillip.ward@gmail.com>
2021-02-19 18:02:37 -0500

    Added Julia scripts to convert VCF position coordinates

--- code/convert_chroms_to_parts.jl
+++ code/convert_chroms_to_parts.jl
@@ -1,23 +1,19 @@
 #!/usr/bin/env julia
 
 #=
-Convert VCF file from chromosome parts or sub-feature coordinates to full
-chromosome coordinates
+Convert VCF file from chromosome pseudomolecule coordinates to feature coordinates
 
 This script takes as input a VCF file (compressed with gzip/bgzip or uncompressed)
 and updates the coordinates based on a .bed file. It is intended primarily to
-convert a VCF file with coordinates in terms of chromosome pieces, scaffolds,
-contigs, etc. into full chromosome pseudomolecule coordinates.
-
-This is useful because some programs, notably GATK, cannot handle positions
-higher than approximately 530Mb.
+convert a VCF file with coordinates in terms of full chromosome pseudomolecules
+into smaller scaffolds, contigs, parts, etc.
 
 The .bed file directs the coordinate conversion. This is a tab-delimited file
 (no header) with the following columns:
-  1) Chromosome name (feature will be renamed to this in output)
+  1) Chromosome name (current name of chromosome in VCF)
   2) Feature start position (0-based)
   3) Feature end position (1-based)
-  4) Feature name (current name of feature in VCF)
+  4) Feature name (name will be updated to this in output VCF)
 
 Example of first few rows of a .bed file (using "|" to denote tabs):
 
@@ -25,6 +21,9 @@ Example of first few rows of a .bed file (using "|" to denote tabs):
 1A | 471304005 | 594102056 | chr1A_part2
 1B |         0 | 438720154 | chr1B_part1
 
+NOTE: features cannot be overlapping. If this occurs, any variants occuring within
+multiple features will be discarded with a warning. Similarly, variants that do
+not occur in any feature are also discarded with a warning.
 =#
 
 using CSV
@@ -32,7 +31,7 @@ using DataFrames
 using BGZFStreams
 using Statistics
 
-bed_file = "/home/brian/Downloads/GDrive_downloads/chr_translate.bed"
+bed_file = "/home/brian/Downloads/GDrive_downloads/test_chr_convert.bed"
 in_vcf_file = "/home/brian/Downloads/GDrive_downloads/test.vcf.gz"
 out_vcf_file = "/home/brian/Downloads/GDrive_downloads/test_out.vcf"
 
@@ -52,19 +51,12 @@ end
 
 ## Read in the .bed file and find max. position of each chromosome
 bed = CSV.read(bed_file, DataFrame, header = false, delim = "\t")
-maxes = groupby(bed, :Column1)
-maxes = combine(maxes, :Column3 => maximum => :max)
+rename!(bed, ["chrom", "start", "end", "feature"])
 
 ## Create vector of new contig IDs
 contigs = []
-for i in 1:nrow(maxes)
-    push!(contigs, "##contig=<ID=" * string(maxes.Column1[i]) * ",length=" * string(maxes.max[i]) * ">")
-end
-
-## Create dictionary to translate between sequences
-seq_dict = Dict{AbstractString, Tuple{AbstractString, UInt64}}()
 for i in 1:nrow(bed)
-    seq_dict[bed.Column4[i]] = (bed.Column1[i], bed.Column2[i])
+    push!(contigs, "##contig=<ID=" * string(bed.feature[i]) * ",length=" * string(bed.end[i] - bed.start[i]) * ">")
 end
 
 #### Main Loop ####
@@ -83,13 +75,18 @@ for line in eachline(istream)
         end
     else
         line = split(line, "\t")
-        orig_chrom = line[1]
-        line[1] = seq_dict[orig_chrom][1]
-        line[2] = string(seq_dict[orig_chrom][2] + parse(UInt64, line[2]))
-        #new_chr = seq_dict[line[1]][1]
-        #new_pos = seq_dict[line[1]][2] + parse(UInt64, line[2])
-        #line = [new_chr; string(new_pos); line[3:length(line)]]
-        println(ostream, join(line, "\t"))
+        pos = parse(UInt64, line[2])
+
+        bed_row = bed[(bed.chrom .== line[1]) .& (bed.start .< pos) .& (bed.end .>= pos), :]
+        if nrow(bed_row) == 0
+            println("WARNING: Variant at chrom: " * line[1] * " pos: " * line[2] * " does not occur in any listed feature - skipping")
+        elseif nrow(bed_row) > 1
+            println("WARNING: Variant at chrom: " * line[1] * " pos: " * line[2] * " occurs in multiple features - skipping")
+        else
+            line[1] = bed_row[1, :feature]
+            line[2] = string(pos - bed_row[1, :start])
+            println(ostream, join(line, "\t"))
+        end
     end
 end
 
